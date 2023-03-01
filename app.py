4ad283542480e967d57508776e0ab15f1c132fd3
sherif saad <ebinsaad@sherifs-MBP.cgocable.net>
2023-03-01 10:56:14 -0500

    change return message

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return 'This is a to-do app'
 
 
 if __name__ == '__main__':
